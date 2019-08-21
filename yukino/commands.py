66641767d755ecb4551454d127ca0590c51f7f13
HiDannyZhu <chdannyzhu@yahoo.com>
2019-08-21 18:03:57 -0400

    Danny changes

--- yukino/commands.py
+++ yukino/commands.py
@@ -143,7 +143,7 @@ async def play(ctx, url: str):
     voice.source.volume = 0.07
 
     #Prints out the song that is being played
-    nname = name.rsplit("-", 2)
+    nname = name[:-16]
     await ctx.send(f"Playing: {nname[0]}")
     print("playing\n")
 
@@ -182,8 +182,30 @@ async def disconnect(ctx):
 
 @bot.command(pass_context=True)
 async def yt(ctx, url):
-
-	voice_channel = ctx.message.author.voice.voice_channel
-	voice_client = await bot.join_voice_channel(voice_channel)
-
-	player = await voice_client.create_ytdl_player(url)
+ 
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music end or use the 'stop' command")
+        return
+    await ctx.send("Getting everything ready, playing audio soon")
+    print("Someone wants to play music let me get that ready for them...")
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, 'song.mp3')
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+    voice.volume = 100
+    voice.is_playing()
