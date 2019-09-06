419552d6466763968f8154543255fc0857d8e2fb
HiDannyZhu <chdannyzhu@yahoo.com>
2019-09-05 22:02:28 -0400

    Updating Branch

--- yukino/commands.py
+++ yukino/commands.py
@@ -16,12 +16,12 @@ audiodir = 'yukino/data/audiofiles/'
 webmdir = 'yukino/data/webmcache/'
 audiotype = [
     'Openings',
-    'Endings', 
+    'Endings',
     'Inserts'
 ]
 
 audiofileext = [
-    '.mp3', 
+    '.mp3',
     '.webm',
     '.wav'
 ]
@@ -31,7 +31,7 @@ dataservers = {
     "Openings.moe": "openings.moe"
 }
 
-#youtube download options
+# youtube download options
 ydl_opts = {
     'format': 'bestaudio/best',
     'postprocessors': [{
@@ -41,16 +41,18 @@ ydl_opts = {
     }],
 }
 
+
 def converttoaudio(ftype, url, filename=secrets.token_hex(16), directory=audiodir, ext=audiofileext[0]):
     '''
     filename is defaulted to a 16-hex randomized string;; filename must be a string
-    directory is defaulted to audiodir;; directory must be a string 
+    directory is defaulted to audiodir;; directory must be a string
     ext is defaulted to mp3;; must be a string preceiding with '.'
     ftype is a string value that organizes audiofiles into subdirectories
     ...
     this function returns a discord.File object, None if error
     '''
-    AudioSegment.converter = which("ffmpeg") #defaulting ffmpeg as a converter
+    AudioSegment.converter = which(
+        "ffmpeg")  # defaulting ffmpeg as a converter
     try:
         audiofile = directory+ftype+'/'+filename+ext
         webmfile = webmdir+filename+audiofileext[1]
@@ -63,53 +65,70 @@ def converttoaudio(ftype, url, filename=secrets.token_hex(16), directory=audiodi
                     file.write(r.content)
             audio = AudioSegment.from_file(webmfile, 'webm')
             audio.export(audiofile, format=ext[1:])
-            cachemanager(webmdir) # deleting webm files cz they r huge;; audio files are tiny in comparison
+            # deleting webm files cz they r huge;; audio files are tiny in comparison
+            cachemanager(webmdir)
         return discord.File(audiofile, spoiler=False)
     except:
         return None
+
 
 def cachemanager(directory):
     cachesize = 0
     for file in os.listdir(directory):
         cachesize += os.path.getsize(directory+file)
     if cachesize >= 200000000:
-        os.remove(directory+os.listdir(directory)[random.randint(0,3)])
+        os.remove(directory+os.listdir(directory)[random.randint(0, 3)])
         print('cacheresized')
-    
+
+
 @bot.command()
 async def test(ctx, *, arg):
     await ctx.send(arg)
 
+
 @bot.command()
 async def clear(ctx, amount=1):
     await ctx.channel.purge(limit=amount)
+
 
 @bot.command()
 async def ping(ctx):
     pong = bot.latency * 1000
     await ctx.send('the current ping is {:.5}ms'.format(pong))
 
+
 @bot.command(aliases=['conv', 'cv', 'convert'])
 async def _convert(ctx, url):
-    async with ctx.typing():
-        try:
-            if dataservers["AnimeThemes"] in url:
-                filename = url.split('/')[-1].split('.')[0] #extracts videoname
-                ftype = audiotype[0] if '-OP' in url else audiotype[1]#animetheme only has Openings and Endings
-            audiofile = converttoaudio(ftype=ftype, url=url, filename=filename)
-            if audiofile == None:
-                raise Exception #error thrown by converttoaudio
-            await ctx.send(
-                'File conversion complete! The following is the converted {} file of {}'
-                .format(audiofileext[0][1:], filename), file=audiofile)
-        except:
-            ctx.send('∑（｡･Д･｡）??? The File Conversion Has Failed. ( •᷄ὤ•᷅)？')
+	async with ctx.typing():
+		try:
+			print("Before Data Servers\n")
+			if dataservers["AnimeThemes"] in url:
+				# extracts videoname
+				filename = url.split('/')[-1].split('.')[0]
+				# animetheme only has Openings and Endings
+				ftype = audiotype[0] if '-OP' in url else audiotype[1]
+				audiofile = converttoaudio(ftype=ftype, url=url, filename=filename)
+			if audiofile == None:
+				print("We reachedd Exception\n")
+				raise Exception  # error thrown by converttoaudio
+			await ctx.send(	
+				'File conversion complete! The following is the converted {} file of {}'
+				.format(audiofileext[0][1:], filename), file=audiofile)
+			# Helper Functions at the bottom
+			print("Before Connect")
+			await connect(ctx)
+			print("Before Play but After Connect")
+			#await playSong(ctx, filename)
+			print("After Play")
+		except:
+			await ctx.send('∑（｡･Д･｡）??? The File Conversion Has Failed. ( •᷄ὤ•᷅)？')
+
 
 @bot.command()
 async def anilist(ctx):
     query = '''
     query ($id: Int) {
-    Media (id: $id, type: ANIME) { 
+    Media (id: $id, type: ANIME) {
         id
         title {
         romaji
@@ -119,82 +138,103 @@ async def anilist(ctx):
     }
     }
     '''
-	
+
     # Define our query variables and values that will be used in the query request
     variables = {
-        'id': 10087#,
-        #'search': 'Katanagatari'
+        'id': 10087  # ,
+        # 'search': 'Katanagatari'
     }
     url = 'https://graphql.anilist.co'
 
-    response = requests.post(url, json={'query': query, 'variables': variables})
+    response = requests.post(
+        url, json={'query': query, 'variables': variables})
     await ctx.send(response.json())
-	
-@bot.command(pass_context=True, aliases=['p', 'pla'])
+
+
+@bot.command(aliases=['p', 'pla'])
 async def play(ctx, url: str):
 
-	global voice
+    await connect(ctx)
 
-	# gets the Channel we're in
-	channel = ctx.message.author.voice.channel
+    # If Song.mp3 exists, True.
+    # If Song.mp3 doesn't exist, False
+    song_there = os.path.isfile("song.mp3")
 
-	voice = get(bot.voice_clients, guild=ctx.guild)
+    try:
+        # If Song is in use, we go to except statement
+        if song_there:
+            os.remove("song.mp3")
+            print("Removed old song file")
+    except PermissionError:
+        print("Trying to delete song file, but it's being played")
+        await ctx.send("ERROR: Music playing")
+        return
 
-	# If Yukino is already in a different voice channel and is connected, move to current channel
-	if voice and voice.is_connected():
-		await voice.move_to(channel)
-	else:
-		voice = await channel.connect()
+    await ctx.send("Getting everything ready now")
 
-	await ctx.send(f"Joined {channel}")
+    # Passing in our options to youtube_dl
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("Downloading audio now\n")
 
-	# If Song.mp3 exists, True.
-	# If Song.mp3 doesn't exist, False 
-	song_there = os.path.isfile("song.mp3")
+        # Downloads audio to current directory
+        ydl.download([url])
 
-	try:
-		#If Song is in use, we go to except statement
-	    if song_there:
-	        os.remove("song.mp3")
-	        print("Removed old song file")
-	except PermissionError:
-	    print("Trying to delete song file, but it's being played")
-	    await ctx.send("ERROR: Music playing")
-	    return
+	#Searches current directory where code was executed (Current Directory is run.py location)
+    for file in os.listdir("./"):
+        # Rename Song file to uniformed name
+        if file.endswith(".mp3"):
 
-	await ctx.send("Getting everything ready now")
+            # save name of file
+            name = file
+            print(f"Renamed File: {file}\n")
+            os.rename(file, "song.mp3")
 
-	# Passing in our options to youtube_dl
-	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-	    print("Downloading audio now\n")
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    # Plays the song and "after" displays message when its done
+    voice.play(discord.FFmpegPCMAudio("song.mp3"),
+               after=lambda e: print("Song done!"))
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.50
 
-	    #Downloads audio
-	    ydl.download([url])
+    # Prints out the song that is being played
+    nname = name.rsplit("-", 2)
+    await ctx.send(f"Playing: {nname[0]}")
+    print("playing\n")
 
-	for file in os.listdir("./"):
-		#Rename Song file to uniformed name
-	    if file.endswith(".mp3"):
+# Helper Functions
 
-	    	#save name of file
-	        name = file
-	        print(f"Renamed File: {file}\n")
-	        os.rename(file, "song.mp3")
+'''
+Purpose: Helper function that  
+Parameters: Ctx, file name of the converted file
+'''
+async def playSong(ctx, filename: str):
 
-	# Plays the song and "after" displays message when its done
-	voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print("Song done!"))
-	voice.source = discord.PCMVolumeTransformer(voice.source)
-	voice.source.volume = 0.07
+    # Change Directory to where Song File is downloaded to
+    os.chdir("yukino/data/audiofiles/Openings")
 
-	#Prints out the song that is being played
-	nname = name.rsplit("-", 2)
-	await ctx.send(f"Playing: {nname[0]}")
-	print("playing\n")
+    voice = get(bot.voice_clients, guild=ctx.guild)
 
+	#Adds .mp3 to the end of file name
+    trueFileName = filename + ".mp3"
+    print("The File Name is:", trueFileName)
 
-@bot.command()
+	#Finds the correct file and plays it
+    voice.play(discord.FFmpegPCMAudio(trueFileName),
+               after=lambda e: print("Song done!"))
+
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.50
+
+	#Sends back to Channel the name of file
+    nname = filename.rsplit("-", 2)
+    await ctx.send(f"Playing: {nname[0]}")
+
+'''
+Purpose: Helper function that Connects to the channel User who called Command is in 
+Parameters: Ctx
+'''
 async def connect(ctx):
-	global voice
-	
+
 	# gets the Channel we're in
 	channel = ctx.message.author.voice.channel
 
@@ -205,79 +245,25 @@ async def connect(ctx):
 		await voice.move_to(channel)
 	else:
 		voice = await channel.connect()
-
 	await ctx.send(f"Joined {channel}")
+
 
 @bot.command(aliases=['dc', 'disc'])
 async def disconnect(ctx):
 
-	# gets the Channel we're in
-	channel = ctx.message.author.voice.channel
+    # gets the Channel we're in
+    channel = ctx.message.author.voice.channel
 
-	voice = get(bot.voice_clients, guild=ctx.guild)
-	# If Yukino is already in a different voice channel and is connected, disconnect it
-	if voice and voice.is_connected():
-		await voice.disconnect()
-        # await ctx.send(f"Yukino Bot has been Killed in Channel {channel} D:")
-	else:
-		# If its not in a channel at all
-		await ctx.send("Baka. Yukino is not in a Channel!")
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    # If Yukino is already in a different voice channel and is connected, disconnect it
+    if voice and voice.is_connected():
+        await voice.disconnect()
+    # await ctx.send(f"Yukino Bot has been Killed in Channel {channel} D:")
+    else:
+        # If its not in a channel at all
+        await ctx.send("Baka. Yukino is not in a Channel!")
 
-@bot.command(pass_context=True)
+
+@bot.command()
 async def _test(ctx, url):
-	global voice
-
-	# gets the Channel we're in
-	channel = ctx.message.author.voice.channel
-
-	voice = get(bot.voice_clients, guild=ctx.guild)
-
-	# If Yukino is already in a different voice channel and is connected, move to current channel
-	if voice and voice.is_connected():
-		await voice.move_to(channel)
-	else:
-		voice = await channel.connect()
-
-	await ctx.send(f"Joined {channel}")
-
-	# If Song.mp3 exists, True.
-	# If Song.mp3 doesn't exist, False 
-	song_there = os.path.isfile("song.mp3")
-
-	try:
-		#If Song is in use, we go to except statement
-	    if song_there:
-	        os.remove("song.mp3")
-	        print("Removed old song file")
-	except PermissionError:
-	    print("Trying to delete song file, but it's being played")
-	    await ctx.send("ERROR: Music playing")
-	    return
-
-	await ctx.send("Getting everything ready now")
-
-	# Passing in our options to youtube_dl
-	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-	    print("Downloading audio now\n")
-
-	    #Downloads audio
-	    ydl.download([url])
-
-	for file in os.listdir("./"):
-		#Rename Song file to uniformed name
-	    if file.endswith(".mp3"):
-
-	    	#save name of file
-	        name = file
-	        print(f"Renamed File: {file}\n")
-	        os.rename(file, "song.mp3")
-
-	# Plays the song and "after" displays message when its done
-	voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print("Song done!"))
-	voice.source = discord.PCMVolumeTransformer(voice.source)
-	voice.source.volume = 0.07
-
-	#Prints out the song that is being played
-	nname = name.rsplit("-", 2)
-	await ctx.send(f"Playing: {nname[0]}")
-	print("playing\n")
+    print("DannyIsGod")
